6ae67d6614a450ee3a1a571f7c2f30804b730627
Arthur Araujo Goncalves Meira <1088787@senacminas.edu.br>
2025-10-27 14:42:18 -0300

    projeto-certo

---
+++ login/login.js
@@ -0,0 +1,13 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // impede o refresh da página
+
+  const tipo = document.getElementById("tipo").value;
+
+  if (tipo === "paciente") {
+    window.location.href = "../paciente/paciente.html"; 
+  } else if (tipo === "medico") {
+    window.location.href = "../medico/medico.html"; 
+  } else {
+    alert("Por favor, selecione um tipo de usuário.");
+  }
+});
