4f16971d257c4c963dc1e30e69b66952654c4c66
Mateus Pereira de Oliveira <1062135@senacminas.edu.br>
2025-10-27 16:30:55 -0300

    resolvendo pwa

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,29 @@
-const CACHE_NAME = "pi-cache-v2";
+const CACHE_NAME = "pi-cache-v3";
 const urlsToCache = [
-  "./consulta/index.html",
-  "./consulta/styles.css",
-  "./consulta/script.js",
-  "./login/login.html",
-  "./login/login.css",
-  "./login/login.js",
-  "./cadastro/cadastro.html",
-  "./cadastro/cadastro.css",
-  "./cadastro/cadastro.js",
-  "./medico/medico.html",
-  "./medico/medico.css",
-  "./medico/medico.js",
-  "./paciente/paciente.html",
-  "./paciente/paciente.css",
-  "./paciente/paciente.js"
+  "/index.html",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png",
+
+  "/consulta/consulta.html",
+  "/consulta/styles.css",
+  "/consulta/script.js",
+
+  "/login/login.html",
+  "/login/login.css",
+  "/login/login.js",
+
+  "/cadastro/cadastro.html",
+  "/cadastro/cadastro.css",
+  "/cadastro/cadastro.js",
+
+  "/medico/medico.html",
+  "/medico/medico.css",
+  "/medico/medico.js",
+
+  "/paciente/paciente.html",
+  "/paciente/paciente.css",
+  "/paciente/paciente.js"
 ];
 
 // Instala o service worker e faz cache inicial
@@ -43,7 +52,7 @@ self.addEventListener("activate", event => {
   );
 });
 
-// Intercepta as requisições e responde com o cache
+// Intercepta requisições e responde com cache ou rede
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
